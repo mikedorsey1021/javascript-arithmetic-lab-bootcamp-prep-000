10a5b01845c1d079f41d54873eca89aa256bc7a2
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-03-26 01:27:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function add (a,b){
+  return a + b
+}
+
+function subtract (a,b){
+  return a - b
+}
+
+function multiply (a,b){
+  return a * b
+}
+
+function divide (a,b){
+  return a / b
+}
+
+function inc (n){
+  return n++
+}
+
+function dec (n){
+  return n--
+}
+
+function makeInt (n){
+  if (parseInt(n) === NaN){
+    return parseInt(n)
+  }
+}
+
+function preserveDecimal (n){
+  if (parseInt(n) === NaN){
+    return parseInt(n)
+  } else {
+    return parseInt(n)
+  }
+}
